eafb8312ae3f7ae31d5a7f6a9a8a4d35e764163f
Ondřej Hošek <ondra.hosek@gmail.com>
2022-07-13 08:51:13 +0200

    TM: Spotify bought Heardle and everything is worse now

--- tampermonkey/heardle-copy-guesses.user.js
+++ tampermonkey/heardle-copy-guesses.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Wordle Copy Guesses - Heardle
 // @namespace    http://ondrahosek.com/tampermonkey/wordle-copy-guesses/heardle
-// @version      0.1
+// @version      0.2
 // @description  Provide a button to quickly copy Heardle guesses into the clipboard.
 // @author       Ondřej Hošek <ondra.hosek@gmail.com>
-// @match        https://www.heardle.app/
+// @match        https://www.spotify.com/heardle/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=www.heardle.app
 // @grant        none
 // ==/UserScript==
@@ -12,11 +12,14 @@
 (function() {
     'use strict';
 
+    var BREAK_SHARE_API = true;
+
     function setUpButton(statsButton) {
         var copyButton = document.createElement("input");
         copyButton.type = "button";
         copyButton.value = "copy guesses";
         copyButton.style.backgroundColor = "var(--color-mg)";
+        copyButton.style.color = "var(--color-fg)";
         statsButton.parentElement.insertBefore(copyButton, statsButton);
 
         copyButton.addEventListener("click", function () {
@@ -31,15 +34,15 @@
                 if (todaysStats.correctAnswer !== undefined) {
                     guessList.push(todaysStats.correctAnswer);
                 } else {
-                    // missing from localStorage; extract from SoundCloud link
-                    var titleRegex = /^Listen to (.+) on SoundCloud$/;
+                    // missing from localStorage; extract from Spotify link
+                    var titleRegex = /^Listen to (.+) by (.+) on Spotify$/;
                     var links = document.getElementsByTagName("a");
                     for (var i = 0; i < links.length; i++) {
                         var matches = titleRegex.exec(links[i].title);
                         if (matches === null) {
                             continue;
                         }
-                        guessList.push(matches[1]);
+                        guessList.push(matches[2] + " - " + matches[1]);
                         break;
                     }
                 }
@@ -52,12 +55,18 @@
                 copyButton.value = "failed";
             });
         });
+
+        if (BREAK_SHARE_API) {
+            // also break the share API
+            // (Windows and Chrome don't allow clipboard access through it)
+            navigator.share = null;
+        }
     }
 
     var waitAndGiveStatsButton;
     var statsButtonCount = 0;
     waitAndGiveStatsButton = function () {
-        var statsButton = document.querySelector("header > div > div > div.justify-end > button");
+        var statsButton = document.querySelector("div > div > div > div > h1 + button");
         if (statsButton === null && statsButtonCount < 5) {
             statsButtonCount += 1;
             window.setTimeout(waitAndGiveStatsButton, 200);
